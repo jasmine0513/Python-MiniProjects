13142b296cdb264b4bd414c279f6bacb7211bb35
Jasmine Carrion <152462643+jasmine0513@users.noreply.github.com>
2026-02-10 16:03:01 -0500

    Add files via upload

---
+++ simplemonths.py
@@ -0,0 +1,11 @@
+
+months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+half = months[6]
+start = 9
+term = 3
+print(half.upper())
+print(months[-1].lower())
+print(months[start-1])
+print(months[(start+term-1)%12])
+
